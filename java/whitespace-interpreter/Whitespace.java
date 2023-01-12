5f469fb048b60523d407fcdf3035a1d397c095b0
That_Guy977 <sirawit.andy977@gmail.com>
2023-01-13 02:06:31 +0700

    Update java/whitespace-interpreter
    Fully implement tokenizer
    Add call stack
    Partially implement interpreter

--- java/whitespace-interpreter/Whitespace.java
+++ java/whitespace-interpreter/Whitespace.java
@@ -1,24 +1,21 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 
 public class Whitespace {
     private final Command[] cmds;
-    private final int[] labels;
+    private final Map<Integer, Integer> labels = new HashMap<>();
     private final Stack<Integer> stack = new Stack<>();
     private final Map<Integer, Integer> heap = new HashMap<>();
+    private final Stack<Integer> callstack = new Stack<>();
 
-    static enum IMP {
+    enum IMP {
         STACK (" "),
         ARITH ("\t "),
         HEAP ("\t\t"),
@@ -39,40 +36,54 @@ public class Whitespace {
         }
     }
 
-    static enum Instr {
-        PUSH (" ", IMP.STACK, Param.NUMBER),
-        DUP ("\n ", IMP.STACK, null),
-        COPY ("\t ", IMP.STACK, Param.NUMBER),
-        SWAP ("\n\t", IMP.STACK, null),
-        DROP ("\n\n", IMP.STACK, null),
-        SLIDE ("\t\n", IMP.STACK, Param.NUMBER),
-        ADD ("  ", IMP.ARITH, null),
-        SUB (" \t", IMP.ARITH, null),
-        MUL (" \n", IMP.ARITH, null),
-        DIV ("\t ", IMP.ARITH, null),
-        MOD ("\t\t", IMP.ARITH, null),
-        STORE (" ", IMP.HEAP, null),
-        RETRIEVE ("\t", IMP.HEAP, null),
-        LABEL ("  ", IMP.FLOW, Param.LABEL),
-        CALL (" \t", IMP.FLOW, Param.LABEL),
-        JMP (" \n", IMP.FLOW, Param.LABEL),
-        JZ ("\t ", IMP.FLOW, Param.LABEL),
-        JN ("\t\t", IMP.FLOW, Param.LABEL),
-        RET ("\t\n", IMP.FLOW, null),
-        END ("\n\n", IMP.FLOW, null),
-        PRINTC ("  ", IMP.IO, null),
-        PRINTI (" \t", IMP.IO, null),
-        READC ("\t ", IMP.IO, null),
-        READI ("\t\t", IMP.IO, null);
+    enum Instr {
+        PUSH (IMP.STACK, " ", Param.NUMBER),
+        DUP (IMP.STACK, "\n ", 1),
+        COPY (IMP.STACK, "\t ", Param.NUMBER, -2),
+        SWAP (IMP.STACK, "\n\t", 2),
+        DROP (IMP.STACK, "\n\n", 1),
+        SLIDE (IMP.STACK, "\t\n", Param.NUMBER, -2),
+        ADD (IMP.ARITH, "  ", 2),
+        SUB (IMP.ARITH, " \t", 2),
+        MUL (IMP.ARITH, " \n", 2),
+        DIV (IMP.ARITH, "\t ", 2),
+        MOD (IMP.ARITH, "\t\t", 2),
+        STORE (IMP.HEAP, " ", 2),
+        RETRIEVE (IMP.HEAP, "\t", 1),
+        LABEL (IMP.FLOW, "  ", Param.LABEL),
+        CALL (IMP.FLOW, " \t", Param.LABEL),
+        JMP (IMP.FLOW, " \n", Param.LABEL),
+        JZ (IMP.FLOW, "\t ", Param.LABEL),
+        JN (IMP.FLOW, "\t\t", Param.LABEL),
+        RET (IMP.FLOW, "\t\n"),
+        END (IMP.FLOW, "\n\n"),
+        PRINTC (IMP.IO, "  "),
+        PRINTI (IMP.IO, " \t"),
+        READC (IMP.IO, "\t ", 1),
+        READI (IMP.IO, "\t\t", 1);
 
-
-        private final String pattern;
         private final IMP imp;
+        private final String pattern;
         private final Param param;
-        Instr(String pattern, IMP imp, Param param) {
-            this.pattern = pattern;
+        private final int args;
+
+        Instr(IMP imp, String pattern, Param param, int args) {
             this.imp = imp;
+            this.pattern = pattern;
             this.param = param;
+            this.args = args;
+        }
+
+        Instr(IMP imp, String pattern, Param param) {
+            this(imp, pattern, param, 0);
+        }
+
+        Instr(IMP imp, String pattern, int args) {
+            this(imp, pattern, null, args);
+        }
+
+        Instr(IMP imp, String pattern) {
+            this(imp, pattern, null);
         }
 
         static Instr of(IMP imp, String pattern) {
@@ -88,14 +99,13 @@ public class Whitespace {
         }
     }
 
-    static enum Param {
+    enum Param {
         NUMBER, LABEL;
 
         static int parseNumber(String in) {
             if (in.length() <= 1) return 0;
             int sign = in.charAt(0) == ' ' ? 1 : -1;
-            String trimmed = in.replaceFirst("^. *", "");
-            String bin = Stream.of(trimmed.split("")).map((c) -> c == " " ? "0" : "1").collect(Collectors.joining());
+            String bin = Stream.of(in.substring(1).split("")).map((c) -> c.equals(" ") ? "0" : "1").collect(Collectors.joining());
             int mag = bin.length() <= 31 ? Integer.parseInt(bin, 2) : Integer.MAX_VALUE;
             return mag * sign;
         }
@@ -103,38 +113,36 @@ public class Whitespace {
 
     private static class Command {
         private final Instr instr;
-        private String param;
-        private int paramValue;
+        private int param;
 
         private Command(Instr instr) {
             this.instr = instr;
         }
 
-        private void setParam(String param) {
-            this.param = param;
-        }
-
-        private void setParamValue(int value) {
-            paramValue = value;
+        @Override
+        public String toString() {
+            return instr.param == null ? instr.name() : instr + " " + param;
         }
     }
 
     public Whitespace(String code) throws WhitespaceSyntaxErrorException {
         List<Command> cmds = new ArrayList<>();
         List<String> labels = new ArrayList<>();
-        Map<String, Integer> labelKey = new HashMap<>();
         String impBuffer = "";
         String instrBuffer = "";
         String paramBuffer = "";
-        for (int i = 0; i <= code.length(); i++) {
-            char c = i != code.length() ? code.charAt(i) : '\0';
-            IMP impCollected = IMP.of(impBuffer);
-            Instr instrCollected = Instr.of(impCollected, instrBuffer);
-            if (!" \n\t\0".contains(String.valueOf(c))) continue;
-            if (impCollected == null) impBuffer += c;
-            else if (instrCollected == null) {
-                String instrPartial = instrBuffer;
-                if (!Stream.of(Instr.of(impCollected)).anyMatch((instr) -> instr.pattern.startsWith(instrPartial + c))) {
+        IMP imp = null;
+        Instr instr = null;
+        for (int i = 0; i < code.length(); i++) {
+            boolean fullInstr = false;
+            char c = code.charAt(i);
+            if (!" \n\t".contains(String.valueOf(c))) continue;
+            if (imp == null) {
+                impBuffer += c;
+                imp = IMP.of(impBuffer);
+            } else if (instr == null) {
+                String instrNext = instrBuffer + c;
+                if (Stream.of(Instr.of(imp)).noneMatch((instrCheck) -> instrCheck.pattern.startsWith(instrNext))) {
                     throw new WhitespaceSyntaxErrorException(
                         String.format("Unexpected %s at %d", switch (c) {
                             case ' ' -> "Space";
@@ -144,128 +152,141 @@ public class Whitespace {
                         }, i)
                     );
                 }
-                instrBuffer += c;
-            }
-            else if (instrCollected.param != null && c != '\n')
+                instrBuffer = instrNext;
+                instr = Instr.of(imp, instrBuffer);
+                if (instr != null && instr.param == null) fullInstr = true;
+            } else if (c != '\n') {
                 paramBuffer += c;
-            impCollected = IMP.of(impBuffer);
-            instrCollected = Instr.of(impCollected, instrBuffer);
-            if (instrCollected != null && (instrCollected.param == null || c == '\n')) {
-                Command cmd = new Command(instrCollected);
-                if (instrCollected.param != null)
-                    cmd.setParam(paramBuffer);
-                cmds.add(cmd);
-                if (instrCollected == Instr.LABEL) {
-                    if (labels.contains(cmd.param))
-                        throw new WhitespaceSyntaxErrorException(String.format("Duplicate label '%s'", replaceWS(cmd.param)));
-                    labels.add(cmd.param);
+            } else {
+                fullInstr = true;
+            }
+            if (fullInstr) {
+                Command cmd = new Command(instr);
+                if (instr.param == Param.NUMBER)
+                    cmd.param = Param.parseNumber(paramBuffer);
+                if (instr.param == Param.LABEL) {
+                    int label = labels.indexOf(paramBuffer);
+                    if (label != -1) cmd.param = label;
+                    else {
+                        cmd.param = labels.size();
+                        labels.add(paramBuffer);
+                    }
+                    if (instr == Instr.LABEL) {
+                        if (this.labels.containsKey(cmd.param))
+                            throw new WhitespaceSyntaxErrorException(String.format("Duplicate label %d", cmd.param));
+                        this.labels.put(cmd.param, cmds.size() + 1);
+                    }
                 }
-                impBuffer = instrBuffer = paramBuffer = "";
+                cmds.add(cmd);
+                impBuffer = "";
+                instrBuffer = "";
+                paramBuffer = "";
+                imp = null;
+                instr = null;
             }
         }
-        if (!(impBuffer + instrBuffer + paramBuffer).matches("\\x00?"))
+        if (!(impBuffer + instrBuffer + paramBuffer).equals(""))
             throw new WhitespaceSyntaxErrorException("Unexpected EOF");
+        for (int j = 0; j < labels.size(); j++) {
+            if (!this.labels.containsKey(j))
+                throw new WhitespaceSyntaxErrorException(String.format("Undefined label %s", labels.get(j)));
+        }
         this.cmds = cmds.toArray(Command[]::new);
-        this.labels = new int[labels.size()];
-        for (int i = 0; i < cmds.size(); i++) {
-            Command cmd = cmds.get(i);
-            if (cmd.instr.param == Param.NUMBER) {
-                cmd.setParamValue(Param.parseNumber(cmd.param));
-            } else if (cmd.instr.param == Param.LABEL) {
-                if (!labels.contains(cmd.param))
-                    throw new WhitespaceSyntaxErrorException(String.format("Undefined label '%s'", replaceWS(cmd.param)));
-                if (cmd.instr == Instr.LABEL)
-                    labelKey.put(cmd.param, i);
-                cmd.setParamValue(labels.indexOf(cmd.param));
-            }
-        }
-        for (int i = 0; i < labels.size(); i++) {
-            String label = labels.get(i);
-            this.labels[i] = labelKey.get(label);
-        }
     }
 
     private void exec(Scanner inputSource, int inputLength) {
         inputSource.useDelimiter("\\n?");
-        for (int i = 0; i < cmds.length; i++) {
-            Command cmd = cmds[i];
+        for (int pt = 0; pt < cmds.length; pt++) {
+            Command cmd = cmds[pt];
+            int args = cmd.instr.args >= 0 ? cmd.instr.args : cmd.param - cmd.instr.args - 1;
+            if (stack.size() < args)
+                throw new WhitespaceStackArgsException(cmd, args, stack.size());
             switch (cmd.instr) {
-                case PUSH -> stack.push(cmd.paramValue); //--
-                case DUP -> stack.push(stack.peek()); //--
+                case PUSH -> stack.push(cmd.param);
+                case DUP -> stack.push(stack.peek());
                 case COPY -> {
-                    
-                } //--
+                    Stack<Integer> top = new Stack<>();
+                    for (int i = 0; i < cmd.param; i++) {
+                        top.push(stack.pop());
+                    }
+                    int copy = stack.peek();
+                    for (int i = 0; i < cmd.param; i++) {
+                        stack.push(top.pop());
+                    }
+                    stack.push(copy);
+                stack.push(stack.get(stack.size() - cmd.param - 1));
+                }
                 case SWAP -> {
                     int top = stack.pop();
                     int next = stack.pop();
                     stack.push(top);
                     stack.push(next);
-                } //--
-                case DROP -> stack.pop(); //--
+                }
+                case DROP -> stack.pop();
                 case SLIDE -> {
-                    for (int j = 0; j < cmd.paramValue; j++) {
+                    for (int i = 0; i < cmd.param; i++) {
                         stack.pop();
                     }
-                } //--
+                }
                 case ADD -> {
-                    int top = stack.pop();
-                    int next = stack.pop();
-                    stack.push(next + top);
-                } //--
+                    int right = stack.pop();
+                    int left = stack.pop();
+                    stack.push(left + right);
+                }
                 case SUB -> {
-                    int top = stack.pop();
-                    int next = stack.pop();
-                    stack.push(next - top);
-                } //--
+                    int right = stack.pop();
+                    int left = stack.pop();
+                    stack.push(left - right);
+                }
                 case MUL -> {
-                    int top = stack.pop();
-                    int next = stack.pop();
-                    stack.push(next * top);
-                } //--
+                    int right = stack.pop();
+                    int left = stack.pop();
+                    stack.push(left * right);
+                }
                 case DIV -> {
-                    int top = stack.pop();
-                    int next = stack.pop();
-                    stack.push(next / top);
-                } //--
+                    int right = stack.pop();
+                    int left = stack.pop();
+                    stack.push(left / right);
+                }
                 case MOD -> {
-                    int top = stack.pop();
-                    int next = stack.pop();
-                    stack.push(next % top);
-                } //--
+                    int right = stack.pop();
+                    int left = stack.pop();
+                    stack.push(left % right);
+                }
                 case STORE -> {
                     int value = stack.pop();
                     int key = stack.pop();
                     heap.put(key, value);
-                } //--
+                }
                 case RETRIEVE -> {
                     int key = stack.pop();
-                    stack.push(heap.get(key));
-                } //--
+                    stack.push(heap.getOrDefault(key, 0));
+                }
                 case LABEL -> {}
-                case CALL -> {} //--
-                case JMP -> {
-                    i = labels[cmd.paramValue];
-                } //--
+                case CALL -> {
+                    callstack.push(pt + 1);
+                    pt = labels.get(cmd.param);
+                }
+                case JMP -> pt = labels.get(cmd.param);
                 case JZ -> {
-                    int top = stack.pop();
-                    if (top == 0)
-                        i = labels[cmd.paramValue];
-                } //--
+                    if (stack.pop() == 0)
+                        pt = labels.get(cmd.param);
+                }
                 case JN -> {
-                    int top = stack.pop();
-                    if (top < 0)
-                        i = labels[cmd.paramValue];
-                } //--
-                case RET -> {} //--
+                    if (stack.pop() < 0)
+                        pt = labels.get(cmd.param);
+                }
+                case RET -> pt = callstack.pop();
                 case END -> {
                     return;
-                } //--
-                case PRINTC -> {} //--
-                case PRINTI -> {} //--
+                }
+                case PRINTC -> System.out.print(Character.toChars(stack.pop()));
+                case PRINTI -> System.out.print(stack.pop());
                 case READC -> {} //--
                 case READI -> {} //--
             }
         }
+        throw new WhitespaceRuntimeException("Program terminated without END");
     }
 
     public void exec(InputStream inputSource) {
@@ -276,28 +297,20 @@ public class Whitespace {
         exec(new Scanner(input), input.length());
     }
 
-    private String replaceWS(String ws) {
-        return ws.replace(' ', 'S').replace('\t', 'T').replace('\n', 'N');
-    }
-
     public static void main(String[] args) {
-        // if (args.length == 0 || args.length > 2) {
-        //     System.out.println("Usage: java Whitespace.java \"<file.ws>\" \"[input]\"");
-        //     System.exit(-1);
+        // TODO: Remove
+        args = new String[]{ "/Users/that_guy977/Desktop/Stuff/Github/misc-proj/java/whitespace-polyglot/HelloWorld.java" };
+        if (args.length == 0 || args.length > 2) {
+            System.out.println("Usage: java Whitespace.java \"<file.ws>\" \"[input]\"");
+            System.exit(-1);
+        }
         try {
-            String code =
-            // "\n\n\n\n  \n";
-            Files.readString(Path.of(
-                // args[0]
-                "java/whitespace-polyglot/HelloWorld.java"
-            ));
+            String code = Files.readString(Path.of(args[0]));
             Whitespace ws = new Whitespace(code);
             if (args.length < 2) ws.exec(System.in);
             else ws.exec(args[1]);
-        } catch (
-            WhitespaceSyntaxErrorException
-            err
-        ) {
+            return;
+        } catch (WhitespaceSyntaxErrorException | WhitespaceRuntimeException err) {
             System.err.println(err);
         } catch (NoSuchFileException err) {
             System.err.printf("File '%s' not found.\n", args[0]);
@@ -306,6 +319,7 @@ public class Whitespace {
         } catch (IOException err) {
             System.err.printf("Could not read file '%s'.\n", args[0]);
         }
+        System.exit(-1);
     }
 
     public static class WhitespaceSyntaxErrorException extends Exception {
@@ -313,10 +327,19 @@ public class Whitespace {
             super(message);
         }
     }
-    public static class WhitespaceStackException extends RuntimeException {
+    public static class WhitespaceRuntimeException extends RuntimeException {
+        public WhitespaceRuntimeException(String message) {
+            super(message);
+        }
+    }
+    public static class WhitespaceStackException extends WhitespaceRuntimeException {
         public WhitespaceStackException(String message) {
             super(message);
         }
     }
+    public static class WhitespaceStackArgsException extends WhitespaceStackException {
+        public WhitespaceStackArgsException(Command cmd, int args, int size) {
+            super(String.format("Operation %s requires %d elements; Found %d", cmd, args, size));
+        }
+    }
 }
-
