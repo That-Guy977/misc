469c748274ca4e68f921eb237aa779a96d5784d6
That_Guy977 <sirawit.andy977@gmail.com>
2023-01-13 02:05:15 +0700

    Update java/whitespace-polyglot
    Emulate Whitespace behavior

--- java/whitespace-polyglot/HelloWorld.java
+++ java/whitespace-polyglot/HelloWorld.java
@@ -1,35 +1,30 @@
-import java.util.*; 
- 
 
-class HelloWorld {
+import java.util.*;	
 
- 	static final List<String> MESSAGE = new ArrayList<>();
+class HelloWorld	{
+
+ 	static final Stack<Character> MESSAGE = new Stack<>();
 
  	static {     
-
- 	MESSAGE.addAll(List.of(
-      	 	"\n", 
-    	    	"!",
-   		"d",  	  
-   		"l", 		  
-   		"r",	  	 
-   		"o", 				
-   		"w",	 			
-    	" ",    
-    	 	",",	  
-   		"o", 				
-   		"l", 		  
-   		"l", 		  
-   		"e",  	 	
-   	  	"H"   
-
- 	));
+  MESSAGE.push( '\0' );      
+  MESSAGE.push( '\n' );  	 	 
+  MESSAGE.push( '!' );	    	
+  MESSAGE.push( 'd'	);	  	  
+  MESSAGE.push( 'l'	);	 		  
+  MESSAGE.push( 'r'	);		  	 
+  MESSAGE.push( 'o'	);	 				
+  MESSAGE.push( 'w'	);		 			
+  MESSAGE.push( ' '	);    
+  MESSAGE.push( ',' );	 		  
+  MESSAGE.push( 'o'	);	 				
+  MESSAGE.push( 'l'	);	 		  
+  MESSAGE.push( 'l'	);	 		  
+  MESSAGE.push( 'e'	);	  	 	
+  MESSAGE.push( 'H'	);  	   
 
  	}
 
  	public static void main(String[] args) { 
-
- 	    Collections.reverse( MESSAGE );
 
  	  	print();
 
@@ -39,12 +34,12 @@ class HelloWorld {
 
 
   static void print()	{
-	System.out.
-   print(
- 
-	String.join("", MESSAGE)
+	System.out.print(
+  MESSAGE.pop() );
+ MESSAGE.push(MESSAGE.peek());
+if	(MESSAGE.pop() =='\0')return;
 
- );
+ print();
   }	
 }
         
