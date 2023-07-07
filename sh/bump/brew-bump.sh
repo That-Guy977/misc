de647284328143c33668f3eaa2aefba968d1637c
That_Guy977 <sirawit.andy977@gmail.com>
2023-07-07 17:59:18 +0700

    sh/bump - brew-bump: rework cleanup

--- sh/bump/brew-bump.sh
+++ sh/bump/brew-bump.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 if ! [ -d "$(xcode-select -p)" ]; then echo "Xcode at path '$(xcode-select -p)' not found"; exit 1; fi
-brew update \
-&& HOMEBREW_NO_INSTALL_CLEANUP=1 brew upgrade \
-&& brew cleanup \
-&& brew autoremove
+brew update && HOMEBREW_NO_INSTALL_CLEANUP=1 brew upgrade
+brew cleanup
