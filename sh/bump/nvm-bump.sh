1032430213f4f08ef0abab4699227b88a77bfd17
That_Guy977 <sirawit.andy977@gmail.com>
2023-07-23 20:17:25 +0700

    sh/bump - nvm-bump: refactor
    commit 0801d13cd669c526635adddd8faf4b1fb83ffa4d
    Author: That_Guy977 <sirawit.andy977@gmail.com>
    Date:   Sun Jul 23 20:16:03 2023 +0700
    
        sh/bump - nvm-bump: normalize quotes
    
    commit a0281b0e439ce2a17e4ad97b9d082e7add937e0e
    Author: That_Guy977 <sirawit.andy977@gmail.com>
    Date:   Sun Jul 23 20:15:49 2023 +0700
    
        sh/bump - nvm-bump: use `source`
    
    commit e35208f51a94485b46e4fa475d3df297b2c684a0
    Author: That_Guy977 <sirawit.andy977@gmail.com>
    Date:   Sun Jul 23 20:15:28 2023 +0700
    
        sh/bump - nvm-bump: rework cleanup
    
        Add `cache clear`
        Remove `use node`
        Add instructions for switching active version

--- sh/bump/nvm-bump.sh
+++ sh/bump/nvm-bump.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 if ! [ -d "$HOME/.nvm" ]; then echo "NVM at path '$HOME/.nvm' not found"; exit 1; fi
-. "$HOME/.nvm/nvm.sh"
+source "$HOME/.nvm/nvm.sh"
 versions="$(nvm ls --no-colors --no-alias)"
-major=$(echo "$versions" | sed -E 's/^(->)? +v([[:digit:]]+)\.[[:digit:]]+\.[[:digit:]]+ \*$/\2/' | uniq)
+major="$(echo "$versions" | sed -E 's/^(->)? +v([[:digit:]]+)\.[[:digit:]]+\.[[:digit:]]+ \*$/\2/' | uniq)"
 for v in $major; do
   nvm install "$v" --latest-npm
-  extra=$(nvm ls "$v" --no-colors | sed -E -e '$d' -e 's/^ *v([[:digit:].]*) \*$/\1/')
+  extra="$(nvm ls "$v" --no-colors | sed -E -e '$d' -e 's/^ *v([[:digit:].]*) \*$/\1/')"
   for e in $extra; do
     nvm uninstall "$e"
   done
 done
 nvm install "lts/*"
-nvm use node
+nvm cache clear
+echo "To switch active version, run:"
+echo "  nvm use node"
